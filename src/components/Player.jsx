9497385a897c88d3f192cb6670e5415fbe12cad7
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-01 19:13:01 +0100

    Component Instances Work In Isolation!

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((prevState) => !prevState);
+  };
+
+  const inputElement = <input type="text" />;
+  const playerName = <span className="player-name">{name}</span>;
+
   return (
     <li>
-      <span>
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {isEditing ? inputElement : playerName}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
       </span>
     </li>
   );
